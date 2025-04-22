96b80a3857e333e51fcd274db877071046fc454d
depruebas <alex.arenols@depruebas.com>
2025-04-22 06:15:22 +0200

    add log

--- src/app/controllers/pricing.py
+++ src/app/controllers/pricing.py
@@ -1,8 +1,29 @@
 from flask import jsonify
+import os
+import logging
+from datetime import datetime
+from pythonjsonlogger import jsonlogger
+
+os.makedirs("log", exist_ok=True)
+
+# Configurar logger
+logger = logging.getLogger("PricingLogger")
+logger.setLevel(logging.INFO)
+
+log_file = logging.FileHandler("log/pricing.json")
+formatter = jsonlogger.JsonFormatter()
+log_file.setFormatter(formatter)
+logger.addHandler(log_file)
 
 def handle_pricing(description):
 
-    # Send to slack channel
+    log_entry = {
+        "topic": "pricing",
+        "description": description,
+        "date": datetime.utcnow().isoformat()
+    }
+    logger.info(log_entry)
+
     return jsonify({
         "status": "ok",
         "response": f"Ayuda sobre precios: {description}"
